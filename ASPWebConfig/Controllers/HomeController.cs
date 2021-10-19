97b3d1875400aae2e228022df61e80af72596feb
maitripatel135 <maitri_hemangkumar.patel@lcieducation.net>
2021-10-19 14:43:36 -0400

    Add project files.

---
+++ ASPWebConfig/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ASPWebConfig.Models;
+
+namespace ASPWebConfig.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            object name = System.Configuration.ConfigurationManager.AppSettings["Name"];
+
+            return View(name);
+        }
+
+        public ActionResult About()
+        {
+            object name = System.Configuration.ConfigurationManager.AppSettings["Name"];
+
+            return View(name);
+        }
+
+        public ActionResult Contact()
+        {
+            Demo demo = new Demo() { name = "Test Demo" };
+
+            return View(demo);
+        }
+    }
+}
---
+++ ASPWebConfig/Models/Demo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASPWebConfig.Models
+{
+    public class Demo
+    {
+        public string name { get; set; }
+    }
+}
